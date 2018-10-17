e4ee525330221b7eafed05ccc02354b1d7ee9df3
David <davidprc3@gmail.com>
2018-10-16 23:18:02 -0400

    added a prototype of the block minigame with sounds (needing tweaking) block placing functionality and grabbing.

---
+++ Assets/Blocks Items/Scripts/BlockScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour {
+
+	AudioSource aud;
+	Rigidbody rb;
+
+	float pitchRangeLow = .5f;
+	float pitchRangeHight = .8f;
+	float collisionThreshold = .01f;
+	float thresholdWeight = 5f;
+	float maxVol = .6f;
+	// Use this for initialization
+	void Start () {
+		aud = GetComponent <AudioSource>();
+		rb = GetComponent <Rigidbody>();
+		aud.pitch = Random.Range (pitchRangeLow, pitchRangeHight);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnCollisionEnter(Collision collision){
+		float mag = collision.relativeVelocity.magnitude;
+		//if (mag > 10f) {
+		//	print (mag);
+
+		//}
+		if (mag > collisionThreshold) {
+			mag /= 20f;
+			if (mag > maxVol) {
+				mag = maxVol;
+			}
+
+			manageAudio (aud.clip, mag);
+			//manageAudio (aud.clip, Mathf.Sqrt(Mathf.Sqrt(mag))/(collisionThreshold * thresholdWeight) - .3f);
+		} 
+
+
+		//aud.Play ();
+
+
+	}
+
+	void manageAudio(AudioClip a, float volume){
+		//if (!aud.isPlaying) {
+			aud.pitch = Random.Range (pitchRangeLow, pitchRangeHight);
+			aud.volume = volume;
+			//aud.PlayOneShot (a, volume);
+			aud.Play ();
+		//}
+
+	}
+}
---
+++ Assets/Blocks Items/Scripts/Cursor.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+	public Camera camera;
+
+	public GameObject[] blocks;
+	public GameObject[] silhouette;
+	private GameObject cSilhouette;
+	[SerializeField] private int currentBlock = 0;
+
+
+	public struct Grip{
+		public float y;
+		public Vector3 vel;
+		public Vector3 prevPos;
+		public Vector3 offset;
+
+
+	}
+
+	private Grip firstGrip;
+	private GameObject blockHolding;
+	public enum Grab{
+		grabing,
+		holding,
+		releasing,
+		waiting
+
+	}
+	public Grab _grab = Grab.waiting;
+
+	public enum State {
+		place,
+		flood,
+		push,
+		grab
+
+
+	}
+	public State _state = State.place;
+
+
+	public enum Structure {
+		SWITCHING,
+		HOVERING,
+
+
+	}
+	public Structure _type = Structure.SWITCHING;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		switch (_state) {
+		case State.place:
+
+			Indicate();
+			//on left click up
+			if (Input.GetMouseButtonUp (0)) {
+				place ();
+
+			}
+
+			break;
+		case State.flood:
+			Indicate ();
+			if (Input.GetMouseButton(0)) {
+				place ();
+
+			}
+
+			break;
+
+		case State.grab:
+
+			GrabHandler ();
+
+			break;
+
+
+		}
+	}
+
+	void place(){
+		RaycastHit hit;
+		Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+		if (Physics.Raycast (ray, out hit)) {
+			GameObject temp = Instantiate (blocks[currentBlock]);
+			Vector3 pointLocation = hit.point;
+			temp.transform.position = new Vector3 (Mathf.Floor(pointLocation.x),Mathf.Ceil(pointLocation.y),Mathf.Floor(pointLocation.z));
+		}
+
+
+	}
+
+	void Indicate(){
+		switch (_type) {
+
+		case Structure.SWITCHING:
+			
+			if (currentBlock == silhouette.Length) {
+				currentBlock = 0;
+			}
+			if (currentBlock == -1) {
+				currentBlock = silhouette.Length - 1;
+			}
+			Destroy (cSilhouette);
+			cSilhouette = Instantiate (silhouette[currentBlock]);
+			_type = Structure.HOVERING;
+			break;
+
+
+
+		case Structure.HOVERING:
+			RaycastHit hit;
+			Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast (ray, out hit)) {
+
+				Vector3 pointLocation = hit.point;
+				cSilhouette.transform.position = new Vector3 (Mathf.Floor (pointLocation.x), Mathf.Ceil (pointLocation.y), Mathf.Floor (pointLocation.z));
+			}
+
+
+			if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
+				currentBlock++;
+				_type = Structure.SWITCHING;
+			}
+			if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+				currentBlock--;
+				_type = Structure.SWITCHING;
+			}
+
+			break;
+
+
+		}
+
+
+	}
+
+	RaycastHit checkIfBlock(){
+		RaycastHit hit;
+		Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.collider.gameObject.GetComponent<BlockScript> () != null) {
+				return hit;
+			}
+		}
+		return hit;
+
+	}
+
+
+	void GrabHandler(){
+		RaycastHit hit;
+
+		switch (_grab) {
+		case Grab.waiting:
+			if (Input.GetMouseButtonDown (0)) {
+				hit = checkIfBlock ();
+				if(hit.collider != null && hit.collider.gameObject.GetComponent<BlockScript>() != null){
+					blockHolding = hit.collider.gameObject;
+					firstGrip.offset = hit.point - hit.collider.gameObject.transform.position;
+					_grab = Grab.grabing;
+				}
+			}
+			break;
+
+		case Grab.grabing:
+			firstGrip.y = blockHolding.transform.position.y;
+			firstGrip.prevPos = blockHolding.transform.position;
+			firstGrip.vel = Vector3.zero;
+			blockHolding.layer = 2;
+			blockHolding.transform.localScale *= .99f;
+			_grab = Grab.holding;
+
+
+			break;
+		case Grab.holding:
+			Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+			if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
+				firstGrip.y++;
+			}
+			if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+				firstGrip.y--;
+			}
+			if (Physics.Raycast (ray, out hit)) {
+				firstGrip.vel = blockHolding.transform.position - firstGrip.prevPos;
+				firstGrip.prevPos = blockHolding.transform.position;
+				blockHolding.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+				Vector3 pointLocation = hit.point;
+				blockHolding.transform.rotation = Quaternion.identity;
+				//blockHolding.transform.position = new Vector3 (pointLocation.x, firstGrip.y, pointLocation.z);
+				//blockHolding.transform.position = new Vector3(Mathf.Floor (pointLocation.x), firstGrip.y, Mathf.Floor (pointLocation.z));
+				//blockHolding.transform.position = Vector3.Lerp (blockHolding.transform.position,new Vector3(pointLocation.x, firstGrip.y, pointLocation.z), .5f);
+				blockHolding.transform.position = new Vector3 (pointLocation.x + firstGrip.offset.x, firstGrip.y, pointLocation.z + firstGrip.offset.z);
+				//blockHolding.transform.position = new Vector3 (pointLocation.x, firstGrip.y, pointLocation.z);
+
+			}
+
+			if (Input.GetMouseButtonUp (0)) {
+				_grab = Grab.releasing;
+			}
+
+			break;
+		case Grab.releasing:
+			blockHolding.transform.localScale /= .99f;
+			blockHolding.GetComponent<Rigidbody> ().velocity = firstGrip.vel;
+			blockHolding.layer = 0;
+			blockHolding = null;
+			_grab = Grab.waiting;
+
+			break;
+
+		}
+
+
+	}
+}
