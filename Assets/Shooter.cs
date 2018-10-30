02bd6d40c7595de848d518b980bedfcf6c90a7ae
NoahZr <n.zucker1@gmail.com>
2018-10-29 20:21:00 -0400

    backup copy

---
+++ Assets/Shooter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+    public float launchSpeed;
+    public LayerMask interactionMask;
+    public Transform spawnPoint;
+    //list of random objects it can shoot
+    public List<GameObject> possibleObjsToShoot;
+    //list of sounds the objects might emit
+    public List<AudioClip> possibleAudioToEmit;
+    GameObject selectedObject;
+    AudioClip selectedClip;
+    float coolDown;
+	// Use this for initialization
+	void Start () {
+        selectedObject = possibleObjsToShoot[Random.Range(0, possibleObjsToShoot.Count)];
+        selectedClip = possibleAudioToEmit[Random.Range(0, possibleAudioToEmit.Count)];
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0)&& Physics.Raycast(ray, out hit,100, interactionMask)&& coolDown>.01f)
+        {
+            GameObject g = Instantiate(selectedObject, spawnPoint.position, Quaternion.Euler(new Vector3(Random.Range(0f, 259f), Random.Range(0f, 259f), Random.Range(0f, 259f)))) as GameObject;
+            //g.AddComponent<Rigidbody>();  
+            g.GetComponent<Rigidbody>().velocity = (hit.point - g.transform.position).normalized * launchSpeed;
+            coolDown = 0;
+        }
+        else
+        {
+            coolDown += Time.deltaTime;
+        }
+		
+	}
+}
---
+++ Assets/SoundBoard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBoard : MonoBehaviour {
+    public Transform leftMost; //extreme
+    public Transform rightMost; //axis
+    public Transform upperRightMost; //upper one
+    public Transform target;
+    public float minRepeatRate;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator MakeSound()
+    {
+        audioSource.pitch = Mathf.Lerp(0,1,Mathf.Clamp01((target.position.y-rightMost.position.y)/(upperRightMost.position.y-rightMost.position.y)));
+        audioSource.PlayOneShot(audioClip);
+        yield return new WaitForSeconds(.5f);
+        // yield return new WaitForSeconds(Mathf.Lerp(0, 1, Mathf.Clamp01(Vector3.Distance(rightMost.position, target.position) / Vector3.Distance(rightMost.position, upperRightMost.position))));
+        StartCoroutine("MakeSound");
+        yield return new WaitForEndOfFrame();
+    }
+}
