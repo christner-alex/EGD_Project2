5409cf91a65d6eb9a0b2785de11edf992447a1f7
David <davidprc3@gmail.com>
2018-10-29 20:43:46 -0400

    baloon scene has randomized audio (within reason)

--- Assets/PopOnNeedleTouch.cs
+++ Assets/PopOnNeedleTouch.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PopOnNeedleTouch : MonoBehaviour {
+	public AudioClip[] possibleSounds;
     public AudioClip popSound;
     public float volume;
+	public int current = 0;
 	// Use this for initialization
 	void Start () {
         GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, Random.Range(1f, -1f));
+		popSound = possibleSounds [Random.Range (0, possibleSounds.Length)];
 	}
 	
 	// Update is called once per frame
