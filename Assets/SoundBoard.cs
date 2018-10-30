7e684006c3bbe874e3817e505424c92f6fe43aaa
NoahZr <n.zucker1@gmail.com>
2018-10-29 20:43:27 -0400

    added theremin

--- Assets/SoundBoard.cs
+++ Assets/SoundBoard.cs
@@ -12,7 +12,7 @@ public class SoundBoard : MonoBehaviour {
     public AudioClip audioClip;
 	// Use this for initialization
 	void Start () {
-		
+        StartCoroutine("MakeSound");
 	}
 	
 	// Update is called once per frame
@@ -21,9 +21,9 @@ public class SoundBoard : MonoBehaviour {
 	}
     IEnumerator MakeSound()
     {
-        audioSource.pitch = Mathf.Lerp(0,1,Mathf.Clamp01((target.position.y-rightMost.position.y)/(upperRightMost.position.y-rightMost.position.y)));
+        audioSource.pitch = .1f+Mathf.Lerp(0,1,Mathf.Clamp01((target.position.y-rightMost.position.y)/(upperRightMost.position.y-rightMost.position.y)));
         audioSource.PlayOneShot(audioClip);
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(.05f + Mathf.Lerp(0, 1, Mathf.Clamp01((target.position.x - rightMost.position.x) / (leftMost.position.x - rightMost.position.x)))*.1f);
         // yield return new WaitForSeconds(Mathf.Lerp(0, 1, Mathf.Clamp01(Vector3.Distance(rightMost.position, target.position) / Vector3.Distance(rightMost.position, upperRightMost.position))));
         StartCoroutine("MakeSound");
         yield return new WaitForEndOfFrame();
